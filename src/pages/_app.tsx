22c12a5dc95a43379c3b3ed292494325f6137b3a
YagoPassos <yp.anjos@hotmail.com>
2021-02-23 22:00:58 -0300

    babelrc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import { GlobalStyles } from '../styles/GlobalStyles'
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import ExperienceBar from '../components/ExperienceBar'
 import { GlobalStyles } from '../styles/GlobalStyles'
 
-
-
 export default function Home() {
   return (
     <div>
